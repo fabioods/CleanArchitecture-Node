90aef89653fbebb0ecc9cab21fbcce39a41f6e02
Fábio dos Santos <fah_ds@live.com>
2021-05-02 17:08:25 -0300

    refactor: movido arquivos duplicados para um arquivo de helper

--- src/presentation/controllers/signupController.ts
+++ src/presentation/controllers/signupController.ts
@@ -1,18 +1,11 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../error/missing-param-error'
+import { badRequest } from '../helpers/http-helper'
 
 export default class SignUpController {
   handle(httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name)
-      return {
-        statusCode: 400,
-        body: new MissingParamError('name'),
-      }
-    if (!httpRequest.body.email)
-      return {
-        statusCode: 400,
-        body: new MissingParamError('email'),
-      }
+    if (!httpRequest.body.name) return badRequest(new MissingParamError('name'))
+    if (!httpRequest.body.email) return badRequest(new MissingParamError('email'))
     return {
       statusCode: 200,
       body: {},
