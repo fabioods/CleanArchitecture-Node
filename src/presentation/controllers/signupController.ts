93c4f7690161585692f91273bf6958beb9c0a0eb
Fábio dos Santos <fah_ds@live.com>
2021-05-02 18:05:21 -0300

    refactor: criado interface para Controller e signupController implementando a mesma

--- src/presentation/controllers/signupController.ts
+++ src/presentation/controllers/signupController.ts
@@ -1,8 +1,9 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../error/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
+import { Controller } from '../protocols/Controller'
 
-export default class SignUpController {
+export default class SignUpController implements Controller {
   handle(httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (let index = 0; index < requiredFields.length; index++) {
