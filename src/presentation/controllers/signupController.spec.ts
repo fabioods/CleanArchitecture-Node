da9bdfc6949ab3f8d8c03652ea46b9b6c1815616
Fábio dos Santos <fah_ds@live.com>
2021-05-02 16:13:21 -0300

    test: ensure signupcontroller returns an error if no email was provided and statusCode 200 if everything is ok

--- src/presentation/controllers/signupController.spec.ts
+++ src/presentation/controllers/signupController.spec.ts
@@ -14,4 +14,30 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing Param: name'))
   })
+  it('should be able to return 400 if no email was provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'Fábio',
+        password: 'any_password',
+        password_confirmation: 'any_password',
+      },
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing Param: email'))
+  })
+  it('should be able to return 200 if all fields was provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'Fábio',
+        email: 'fah_ds@Live.com',
+        password: 'any_password',
+        password_confirmation: 'any_password',
+      },
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+  })
 })
