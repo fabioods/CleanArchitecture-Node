0a5db0e17d1e7ac3031916af039f680c48a6c8cf
Fábio dos Santos <fah_ds@live.com>
2021-05-02 16:12:28 -0300

    feat: ensure signupcontroller returns an error if no email was provided and statusCode 200 if everything is ok

--- src/presentation/controllers/signupController.ts
+++ src/presentation/controllers/signupController.ts
@@ -1,8 +1,17 @@
 export default class SignUpController {
   handle(httpRequest: any): any {
+    if (!httpRequest.body.name)
+      return {
+        statusCode: 400,
+        body: new Error('Missing Param: name'),
+      }
+    if (!httpRequest.body.email)
+      return {
+        statusCode: 400,
+        body: new Error('Missing Param: email'),
+      }
     return {
-      statusCode: 400,
-      body: new Error('Missing Param: name'),
+      statusCode: 200,
     }
   }
 }
