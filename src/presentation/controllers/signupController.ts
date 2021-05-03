b907bc28687a9484dd1a8e1d6020c36565b5fea8
Fábio dos Santos <fah_ds@live.com>
2021-05-03 20:01:41 -0300

    feat: garantir que o signupcontroller retorna 500 se o email validator lançar uma excessão

--- src/presentation/controllers/signupController.ts
+++ src/presentation/controllers/signupController.ts
@@ -4,6 +4,7 @@ import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/Controller'
 import { EmailValidator } from '../protocols/EmailValidator'
 import { InvalidParamError } from '../error/invalid-param-error'
+import { ServerError } from '../error/server-error'
 
 export default class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
@@ -13,24 +14,28 @@ export default class SignUpController implements Controller {
   }
 
   handle(httpRequest: HttpRequest): HttpResponse {
-    const { body } = httpRequest
-    const { email } = body
+    try {
+      const { body } = httpRequest
+      const { email } = body
 
-    const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
-    for (let index = 0; index < requiredFields.length; index++) {
-      const field = requiredFields[index]
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field))
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
+      for (let index = 0; index < requiredFields.length; index++) {
+        const field = requiredFields[index]
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
 
-    if (!this.emailValidator.isValid(email)) {
-      return badRequest(new InvalidParamError('email'))
-    }
+      if (!this.emailValidator.isValid(email)) {
+        return badRequest(new InvalidParamError('email'))
+      }
 
-    return {
-      statusCode: 200,
-      body: {},
+      return {
+        statusCode: 200,
+        body: {},
+      }
+    } catch (error) {
+      return { statusCode: 500, body: new ServerError() }
     }
   }
 }
