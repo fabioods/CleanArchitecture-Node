cf0317dc0508567de6200e9cfcca373d4fbdcd8d
Fábio dos Santos <fah_ds@live.com>
2021-05-09 13:05:45 -0300

    feat: garantir que o signupcontroller chama o addAccount com valores corretos

--- src/presentation/controllers/signupController.ts
+++ src/presentation/controllers/signupController.ts
@@ -1,18 +1,22 @@
 import { MissingParamError, InvalidParamError } from '../error'
 import { badRequest, serverError } from '../helpers/http-helper'
 import { Controller, EmailValidator, HttpRequest, HttpResponse } from '../protocols'
+import { AddAccount } from '../../domain/useCases/addAccount'
 
 export default class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator
 
-  constructor(emailValidator: EmailValidator) {
+  private readonly addAccount: AddAccount
+
+  constructor(emailValidator: EmailValidator, addAccount: AddAccount) {
     this.emailValidator = emailValidator
+    this.addAccount = addAccount
   }
 
   handle(httpRequest: HttpRequest): HttpResponse {
     try {
       const { body } = httpRequest
-      const { email, password, passwordConfirmation } = body
+      const { email, password, passwordConfirmation, name } = body
 
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (let index = 0; index < requiredFields.length; index++) {
@@ -27,6 +31,12 @@ export default class SignUpController implements Controller {
       if (!this.emailValidator.isValid(email)) {
         return badRequest(new InvalidParamError('email'))
       }
+
+      this.addAccount.add({
+        email,
+        name,
+        password,
+      })
 
       return {
         statusCode: 200,
