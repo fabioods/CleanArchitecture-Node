78c33c66f04f9a6802828d5d3c6c3bf72bdadfa4
Fábio dos Santos <fah_ds@live.com>
2021-05-14 19:13:54 -0300

    refactor: movido criação do sut para um factory

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -1,23 +1,33 @@
 import { Encrypter } from '../../protocolos/encrypter'
 import { DbAddAccount } from './db-add-account'
 
+interface MakeSut {
+  encrypter: Encrypter
+  dbAccount: DbAddAccount
+}
+
+const makeSut = (): MakeSut => {
+  class EncrypterStub implements Encrypter {
+    async encrypt(value: string): Promise<string> {
+      return 'hashedValue'
+    }
+  }
+
+  const encrypter = new EncrypterStub()
+  const dbAccount = new DbAddAccount(encrypter)
+  return { encrypter, dbAccount }
+}
+
 describe('DB Add Account', () => {
   it('should call Encrypter with correct password', async () => {
-    class EncrypterStub implements Encrypter {
-      async encrypt(value: string): Promise<string> {
-        return 'hashedValue'
-      }
-    }
-
-    const encryptorStub = new EncrypterStub()
-    const sut = new DbAddAccount(encryptorStub)
-    const encryptSpy = jest.spyOn(encryptorStub, 'encrypt')
+    const { encrypter, dbAccount } = makeSut()
+    const encryptSpy = jest.spyOn(encrypter, 'encrypt')
     const accountData = {
       name: 'valid_name',
       email: 'valid_email',
       password: 'valid_password',
     }
-    await sut.add(accountData)
+    await dbAccount.add(accountData)
 
     expect(encryptSpy).toHaveBeenCalledWith('valid_password')
   })
