3470b09a072968a7d1e0eb5a2e61d92e3e452b44
Fábio dos Santos <fah_ds@live.com>
2021-05-14 19:02:50 -0300

    feat: garantir que o encrypter seja chamado com um password correto

---
+++ src/data/usecases/add-account/db-add-account.ts
@@ -0,0 +1,16 @@
+import { AccountModel } from '../../../domain/model/account'
+import { AddAccount, AddAccountModel } from '../../../domain/useCases/addAccount'
+import { Encrypter } from '../../protocolos/encrypter'
+
+export class DbAddAccount implements AddAccount {
+  private readonly encrypter: Encrypter
+
+  constructor(encrypter: Encrypter) {
+    this.encrypter = encrypter
+  }
+
+  async add(account: AddAccountModel): Promise<AccountModel> {
+    const { password } = account
+    await this.encrypter.encrypt(password)
+  }
+}
