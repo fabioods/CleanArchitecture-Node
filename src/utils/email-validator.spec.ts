ff78de7cc48579fe068d5d338f00cb226934f194
Fábio dos Santos <fah_ds@live.com>
2021-05-14 18:33:51 -0300

    refactor: movido new do email validator para um makeSut

--- src/utils/email-validator.spec.ts
+++ src/utils/email-validator.spec.ts
@@ -7,22 +7,26 @@ jest.mock('validator', () => ({
   },
 }))
 
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
 describe('Email validator Adapter', () => {
   it('should be able to return false if validator returns false', () => {
-    const emailValidator = new EmailValidatorAdapter()
+    const emailValidator = makeSut()
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = emailValidator.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
   })
 
   it('should be able to return true if validator returns true', () => {
-    const emailValidator = new EmailValidatorAdapter()
+    const emailValidator = makeSut()
     const isValid = emailValidator.isValid('invalid_email@mail.com')
     expect(isValid).toBe(true)
   })
 
   it('should call validator with correct email', () => {
-    const emailValidator = new EmailValidatorAdapter()
+    const emailValidator = makeSut()
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
     emailValidator.isValid('any_email@mail.com')
     expect(isEmailSpy).toHaveBeenCalledWith('any_email@mail.com')
