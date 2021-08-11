f1b08cf23723881cb65b628c9ae104868f0b7d6a
Fábio dos Santos <fah_ds@live.com>
2021-08-11 10:13:59 -0300

    test: ensure dbaddAccount calls AddAccountRepository with correctValues

--- src/data/usecases/add-account/db-add-account-protocols.ts
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -1,3 +1,4 @@
-export { Encrypter } from '../../protocolos/encrypter'
+export { Encrypter } from '../../protocols/encrypter'
 export { AccountModel } from '../../../domain/model/account'
 export { AddAccount, AddAccountModel } from '../../../domain/useCases/addAccount'
+export { AddAccountRepository } from '../../protocols/add-account-repository'
