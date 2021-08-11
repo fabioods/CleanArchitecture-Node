92cb9014819bd09c30765cf6ad32db4688a77ac8
Fábio dos Santos <fah_ds@live.com>
2021-08-11 11:57:23 -0300

    chore: add jest config for mongodb

--- jest.config.ts
+++ jest.config.ts
@@ -88,7 +88,7 @@ export default {
   // notifyMode: "failure-change",
 
   // A preset that is used as a base for Jest's configuration
-  preset: 'ts-jest',
+  preset: '@shelf/jest-mongodb',
 
   // Run tests from one or more projects
   // projects: undefined,
@@ -162,7 +162,9 @@ export default {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
