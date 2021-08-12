0197d8bf40336ba0377fd8e504ab8aed0a5f4ef2
Fábio dos Santos <fah_ds@live.com>
2021-08-12 10:27:27 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -1,9 +1,13 @@
 import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
 import { AccountModel } from '../../../../domain/model/account'
 import { AddAccountModel } from '../../../../domain/useCases/addAccount'
+import { MongoHelper } from '../helpers/mongodb-helper'
 
 export class AccountMongoRepository implements AddAccountRepository {
-  async add(account: AddAccountModel): Promise<AccountModel> {
-    throw new Error('Method not implemented.')
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    const result = await accountCollection.insertOne(accountData)
+    const account = { ...accountData, id: result.insertedId.toString() }
+    return account
   }
 }
